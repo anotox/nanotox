901784c5a9ea6959859fd501a7f676bbb025ba58
anotox <antoine.saby@gmail.com>
2018-05-03 08:34:42 +0200

    first commit

---
+++ conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [ $1 = "Antoine" ]
+then
+	echo "Salut Antoine !"
+elif [ $1 = "Michel" ]
+then
+	echo "Bien le bonjour Michel"
+elif [ $1 = "Jean" ]
+then 
+	echo "Hé Jean, ça va ?"
+
+else 
+	echo "Jte connais pas, dégage !"
+fi
+
